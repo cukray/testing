10324a0ea2c129692d219eb0da65a29793aeea99
CUK <CUK@CUKs-MacBook-Pro.local>
2018-11-05 00:00:06 -0800

    testing

---
+++ Testing/Testing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Testing
+//
+//  Created by CUK on 11/4/18.
+//  Copyright © 2018 U Keong Cheong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Testing/Testing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Testing
+//
+//  Created by CUK on 11/4/18.
+//  Copyright © 2018 U Keong Cheong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
